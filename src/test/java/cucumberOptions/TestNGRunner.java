76e16c18b37608d73155f47d8c702316fabb65aa
ahmedmaged0a <ahmedmaged0a@gmail.com>
2025-06-30 13:34:38 +0300

    Automation Demo Project With Cucumber

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -3,20 +3,16 @@ package cucumberOptions;
 import io.cucumber.java.After;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import io.cucumber.testng.TestNGCucumberRunner;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Listeners;
 import com.swaglabs.Listeners.TestNGListeners;
-import com.swaglabs.drivers.DriverManager;
-import com.swaglabs.utils.BrowserActions;
+
 
 @Listeners(TestNGListeners.class)
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = {"stepDefinitions", "cucumberOptions"},
-        tags = "@validLogin or @invalidLogin or @Smoke or @Regression",
+        tags = "@Smoke or @Regression",
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/cucumber-pretty.html",
